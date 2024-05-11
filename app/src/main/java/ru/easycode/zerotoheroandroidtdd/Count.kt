f1b15d294d9a19e48aa0607fbb6531c9bdf64848
timon <akkdlyaandroid@gmail.com>
2024-05-11 20:34:57 +0600

    task012

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,22 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+    fun increment(number: String): UiState
+
+    class Base(private val step: Int, private val max: Int) : Count {
+        init {
+            if (step < 1) throw IllegalStateException("step should be positive, but was $step")
+            if (max < 1) throw IllegalStateException("max should be positive, but was $max")
+            if (max < step) throw IllegalStateException("max should be more than step")
+        }
+
+        override fun increment(number: String): UiState {
+            val digits = number.toInt()
+            val result = digits + step
+            if (result + step <= max)
+                return UiState.Base(result.toString())
+            else return UiState.Max(result.toString())
+        }
+
+    }
+}
